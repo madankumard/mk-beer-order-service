5b219d82c4abb38d709d91ef6722f860101445ad
MadanKumar <madan739@gmail.com>
2020-07-05 12:40:15 +0530

    Initiate allocation request

--- src/main/java/mk/springframework/mkbeerorderservice/sm/BeerOrderStateMachineConfig.java
+++ src/main/java/mk/springframework/mkbeerorderservice/sm/BeerOrderStateMachineConfig.java
@@ -18,6 +18,7 @@ import java.util.EnumSet;
 public class BeerOrderStateMachineConfig extends StateMachineConfigurerAdapter<BeerOrderStatusEnum, BeerOrderEventEnum> {
 
     private final Action<BeerOrderStatusEnum, BeerOrderEventEnum> validateOrderAction;
+    private final Action<BeerOrderStatusEnum, BeerOrderEventEnum> allocateOrderRequest;
 
     @Override
     public void configure(StateMachineStateConfigurer<BeerOrderStatusEnum, BeerOrderEventEnum> states) throws Exception {
@@ -35,14 +36,18 @@ public class BeerOrderStateMachineConfig extends StateMachineConfigurerAdapter<B
     public void configure(StateMachineTransitionConfigurer<BeerOrderStatusEnum, BeerOrderEventEnum> transitions) throws Exception {
         transitions.withExternal()
                 .source(BeerOrderStatusEnum.NEW).target(BeerOrderStatusEnum.VALIDATION_PENDING)
-                    .event(BeerOrderEventEnum.VALIDATE_ORDER)
+                .event(BeerOrderEventEnum.VALIDATE_ORDER)
                 .action(validateOrderAction)
-                .and().withExternal()
+            .and().withExternal()
                 .source(BeerOrderStatusEnum.NEW).target(BeerOrderStatusEnum.VALIDATED)
-                    .event(BeerOrderEventEnum.VALIDATION_PASSED)
-                .and().withExternal()
+                .event(BeerOrderEventEnum.VALIDATION_PASSED)
+            .and().withExternal()
                 .source(BeerOrderStatusEnum.NEW).target(BeerOrderStatusEnum.VALIDATION_EXCEPTION)
-                    .event(BeerOrderEventEnum.VALIDATION_FAILED);
+                .event(BeerOrderEventEnum.VALIDATION_FAILED)
+            .and().withExternal()
+                .source(BeerOrderStatusEnum.VALIDATED).target(BeerOrderStatusEnum.ALLOCATION_PENDING)
+                .event(BeerOrderEventEnum.ALLOCATE_ORDER)
+                .action(allocateOrderRequest);
     }
 }
 
---
+++ src/main/java/mk/springframework/mkbeerorderservice/sm/actions/AllocateOrderAction.java
@@ -0,0 +1,38 @@
+package mk.springframework.mkbeerorderservice.sm.actions;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import mk.springframework.mkbeerorderservice.config.JmsConfig;
+import mk.springframework.mkbeerorderservice.domain.BeerOrder;
+import mk.springframework.mkbeerorderservice.domain.BeerOrderEventEnum;
+import mk.springframework.mkbeerorderservice.domain.BeerOrderStatusEnum;
+import mk.springframework.mkbeerorderservice.repositories.BeerOrderRepository;
+import mk.springframework.mkbeerorderservice.services.BeerOrderManagerImpl;
+import mk.springframework.mkbeerorderservice.web.mappers.BeerOrderMapper;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.statemachine.StateContext;
+import org.springframework.statemachine.action.Action;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class AllocateOrderAction implements Action<BeerOrderStatusEnum, BeerOrderEventEnum> {
+
+    private final JmsTemplate jmsTemplate;
+    private final BeerOrderRepository beerOrderRepository;
+    private final BeerOrderMapper beerOrderMapper;
+
+    @Override
+    public void execute(StateContext<BeerOrderStatusEnum, BeerOrderEventEnum> context) {
+        String beerOrderId = (String) context.getMessage().getHeaders().get(BeerOrderManagerImpl.ORDER_ID_HEADER);
+        BeerOrder beerOrder = beerOrderRepository.findOneById(UUID.fromString(beerOrderId));
+
+        jmsTemplate.convertAndSend(JmsConfig.ALLOCATE_ORDER_QUEUE,
+                beerOrderMapper.beerOrderToDto(beerOrder));
+
+        log.debug("Sent Allocation Request for order id: " + beerOrderId);
+    }
+}
