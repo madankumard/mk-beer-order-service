fbde82d1d49489daac9c08cd2d64edc6468c0de2
MadanKumar <madan739@gmail.com>
2020-07-10 12:40:46 +0530

    fixing tasting room problem

--- src/main/java/mk/springframework/mkbeerorderservice/services/TastingRoomService.java
+++ src/main/java/mk/springframework/mkbeerorderservice/services/TastingRoomService.java
@@ -37,7 +37,7 @@ public class TastingRoomService {
     }
 
     @Transactional
-    //@Scheduled(fixedRate = 5000) // run every 5 seconds
+    @Scheduled(fixedRate = 5000) // run every 5 seconds
     public void placeTastingRoomOrder(){
         List<Customer> customerList = customerRepository.findAllByCustomerNameLike(BeerOrderBootStrap.TASTING_ROOM);
 
@@ -45,6 +45,7 @@ public class TastingRoomService {
             doPlaceOrder(customerList.get(0));
         } else {
             log.debug("Too many or too few tasting room customers found");
+            customerList.forEach(customer -> log.debug(customer.toString()));
         }
     }
 
